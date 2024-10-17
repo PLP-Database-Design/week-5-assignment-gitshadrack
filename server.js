51ace6af083ac4e592eb5625a324d2dae080af71
gitshadrack <njerishadrack@gmail.com>
2024-10-18 01:39:09 +0300

    Submission

---
+++ server.js
@@ -0,0 +1,94 @@
+// Import dependencies
+const express = require('express')
+const mysql = require('mysql2')
+const dotenv = require('dotenv')
+
+// Initialize Express application
+const app = express()
+
+// Configure environment variables
+
+dotenv.config()
+
+// Create a MySQL connection object using environment variables
+ const db = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+
+ })
+
+// Test the database connection
+ db.connect((err)=>{
+    if(err){
+       // Log an error message if the connection fails
+      return console.log('Failed to Connection to Mysql: ',err.message)
+    } 
+    // Log a success message if the connection is successful
+    console.log('Succeffully Connected to Mysql: ', db.threadId)
+ })
+
+// Define a basic route
+
+app.get('/', (req, res)=>{
+   res.send('<h1>Hello there</h1>');
+ })
+
+//Retireve all patients endpoint
+
+app.get('/patients', (req, res)=>{
+  const getPatients = "SELECT patient_id,first_name,last_name,date_of_birth FROM patients"
+  db.query(getPatients, (err, data)=>{
+    //if error in fetching data
+    if(err){
+      return res.status(400).send('Failed to Get patients data', err.message)
+        }
+        res.status(200).send(data)
+  })
+})
+
+//Retrive all Proividers
+app.get('/providers', (req, res)=>{
+  const getProviders = "SELECT first_name, last_name, provider_specialty FROM providers"
+  db.query(getProviders, (err, data)=>{
+    // if to get err
+    if(err){
+      return res.status(400).send("Failed to fetch Providers", err.message)
+    }
+    res.status(200).send(data)
+  })
+})
+
+//Filter patients by First Name
+app.get('/firstname', (req, res)=>{
+  const patientsFirstname ="SELECT first_name FROM patients"
+  db.query(patientsFirstname, (err, data)=>{
+// Check fails
+    if(err){
+      return res.status(400).send('Failed to fetch first_name', err.message)
+    }
+    res.status(200).send(data)
+  })
+})
+
+//Retrieve all providers by their specialty
+app.get('/speciality', (req, res)=>{
+  const speciality = "SELECT * FROM providers ORDER BY provider_specialty"
+  db.query(speciality, (err, data)=>{
+
+    if(err){
+      return res.status(400).send('Failed to fetch speciality', err.message)
+    }
+    res.status(200).send(data)
+  })
+})
+
+
+// Start the server and listen on the specified port
+const PORT = 8000
+app.listen(PORT, ()=>{
+  console.log(`Server is Running on http://localhost:${PORT}`)
+})
+
+
